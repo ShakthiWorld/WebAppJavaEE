008f1bef59623c3652bcf25a1d36b13a4dd212be
Shakthi <shakthi15july@gmail.com>
2019-07-16 12:06:34 +0000

    final webapp as on 16/07

---
+++ src/main/java/com/visa/training/web/ShoppingCartServlet.java
@@ -0,0 +1,78 @@
+package com.visa.training.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.stream.Stream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ShoppingCartServlet
+ */
+@WebServlet({ "/bookcart", "/carcart", "/bikecart" })
+public class ShoppingCartServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String uri=request.getRequestURI();
+		HttpSession session=request.getSession();
+		if(uri.endsWith("bookcart"))
+		{
+			String[] selectedBooks=request.getParameterValues("books");
+			session.setAttribute("books", selectedBooks);
+			request.getRequestDispatcher("cars.html").forward(request, response);
+		}
+		else if(uri.endsWith("carcart"))
+		{
+			String[] selectedCars=request.getParameterValues("cars");
+			session.setAttribute("cars", selectedCars);
+			request.getRequestDispatcher("bikes.html").forward(request, response);
+		}
+		else if(uri.endsWith("bikecart"))
+		{
+			String[] selectedBikes=request.getParameterValues("bikes");
+			String[] books=(String[])session.getAttribute("books");
+			String[] cars=(String[])session.getAttribute("cars");
+			
+			PrintWriter pw=response.getWriter();
+			pw.println("<html><body>");
+			pw.println("<h4>books<h4>");
+			pw.println("<ul>");
+			
+			Stream.of(books).map(s->"<li>"+s+"</li>").forEach(pw::println);
+						
+			pw.println("</ul>");
+				
+			pw.println("<h4>cars<h4>");
+			pw.println("<ul>");
+			
+			Stream.of(cars).map(s->"<li>"+s+"</li>").forEach(pw::println);
+						
+			pw.println("</ul>");
+			
+			pw.println("<h4>bikes<h4>");
+			pw.println("<ul>");
+			
+			Stream.of(selectedBikes).map(s->"<li>"+s+"</li>").forEach(pw::println);
+						
+			pw.println("</ul>");
+			
+			
+			pw.println("</body></html>");
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
